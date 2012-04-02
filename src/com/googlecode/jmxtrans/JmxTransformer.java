eb7097bffeea20f194f7c546633f405008b06d50
Scott Wang <scott_wang@hotmail.com>
2012-04-02 19:02:43 +0800

    if not run endlessly, terminate the scheduler in execute standalone mode

--- src/com/googlecode/jmxtrans/JmxTransformer.java
+++ src/com/googlecode/jmxtrans/JmxTransformer.java
@@ -287,10 +287,9 @@ public class JmxTransformer implements WatchedCallback {
 
 		this.processServersIntoJobs(this.serverScheduler);
 
-		// Sleep for 10 seconds to wait for jobs to complete.
-		// There should be a better way, but it seems that way isn't working
-		// right now.
-		Thread.sleep(10 * 1000);
+		if (!this.isRunEndlessly()) {
+			this.serverScheduler.shutdown(true);
+		}
 	}
 
 	/**
